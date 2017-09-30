e4e526c59e92b630b57740adfae27b3fec9c8970
Tomasz Tynor <tynor.t@gmail.com>
2017-09-30 23:57:12 +0200

    Atomowki

---
+++ KoszulkiPowerPuff/KoszulkiPowerPuff/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Powerpuff1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string wieka = "";
+
+
+        public MainWindow()
+        {
+            {
+                this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                string UserName1 = Environment.UserName;
+                string path_directory = @"C:\temp";
+                Watcher.MonitorDirectory(path_directory);
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Move.GetSize(3);
+            //this.Visibility = Visibility.Collapsed;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Move.GetSize(5);
+            //this.Visibility = Visibility.Collapsed;
+
+        }
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            Move.GetSize(7);
+            //this.Visibility = Visibility.Collapsed;
+
+        }
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            Move.GetSize(9);
+            //this.Visibility = Visibility.Collapsed;
+
+        }
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            Move.GetSize(12);
+            //this.Visibility = Visibility.Collapsed;
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ KoszulkiPowerPuff/KoszulkiPowerPuff/Move.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Powerpuff1
+{
+    public class Move : MainWindow
+    {
+        public static string rozmiar = "";
+
+        public static void ChangeName()
+        {
+
+            string UserName1 = Environment.UserName;
+            //FileInfo fInfo = new FileInfo(@"C:\Users\" + UserName1 + @"\Downloads\static.png");
+            FileInfo fInfo = new FileInfo(@"C:\temp\static.png");
+            string data_pliku = fInfo.CreationTimeUtc.ToString("HH_mm_ss");
+            string czas_zapisu = fInfo.LastWriteTimeUtc.ToString("HH_mm_ss");
+            //string path = "C:\\Users\\" + UserName1 + @"\Downloads\static.png";
+            string path = @"C:\temp\static.png";
+            string path2 = @"C:\temp2\koszulka_" + rozmiar + "_" + data_pliku + "_" + czas_zapisu + "_" + UserName1 + ".png";
+
+            try
+            {
+                // This statement ensures that the file is created,
+                // but the handle is not kept.
+                if (File.Exists(path2))
+                    File.Delete(path2);
+
+
+                File.Move(path, path2);
+                Console.WriteLine("{0} was moved to {1}.", path, path2);
+
+                // See if the original exists now.
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("The original file still exists, which is unexpected.");
+                }
+                else
+                {
+                    Console.WriteLine("The original file no longer exists, which is expected.");
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+        public static void GetSize(int v)
+        {
+            rozmiar = v.ToString();
+
+        }
+    }
+}
